cec4e1e195fa552a5d7306d99034d3023df60275
Mehrdad <mehrdadostovar@mehrdad-ostovars-MacBook-Pro.local>
2014-06-06 10:01:11 -0500

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Mehrdad on 6/6/14.
+//  Copyright (c) 2014 Mehrdad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
